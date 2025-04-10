a88fd172f6821ccda492034e02e7b4289bf930a3
QANSIGLIERE <qansigliere@gmail.com>
2025-04-10 20:38:26 +0300

    Stable

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 var { Github_API } = require('./lib/apiIntegration');
 
-// Remove me at the end
-(async function API() {
-    let newIntegration = new Github_API();
-    let request = await newIntegration.getPullRequestComments('QANSIGLIERE', 'githubAPIIntegration');
-
-    console.log(JSON.stringify(request));
-})();
-
 module.exports.Github_API = Github_API;
